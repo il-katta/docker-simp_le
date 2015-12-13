493156d1d743bc4f12db1851103f96ee9e11f5e2
ilkatta <andrea.cattaneo.dev@gmail.com>
2015-12-13 22:47:16 +0000

    change dir before run webserver

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -xe
-# starting wevserver
-mkdir -p /var/www/html
-cd /var/www/html
-webfsd -l /dev/stdout -p 80 -j
+# starting webserver
+(
+    mkdir -p /var/www/html
+    cd /var/www/html
+    webfsd -l /dev/stdout -p 80 -j
+)
 # cmd
 cd /certs
 /usr/local/bin/simp_le --default_root /var/www/html $@
